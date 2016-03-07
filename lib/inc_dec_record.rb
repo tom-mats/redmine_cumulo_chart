3eb562ff72b31b1746db6d9b0c4d5186891338bd
tom-mats <dawn.gray.sky@gmail.com>
2016-03-08 01:54:41 +0900

    fix typo

--- lib/inc_dec_record.rb
+++ lib/inc_dec_record.rb
@@ -3,7 +3,7 @@ module IncDecRecord
     def controller_issues_new_after_save(context={})
     end
     def controller_issues_edit_after_save(context={})
-      c_journal = context[:issue].jounrals.map{|elem| elem.id == context[:journal].id}
+      c_journal = context[:issue].journals.map{|elem| elem.id == context[:journal].id}
       p c_journal
     end
   end
