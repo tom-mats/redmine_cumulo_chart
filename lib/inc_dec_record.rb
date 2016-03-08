a711b320e06fb07e5eacfa9d1581c1c5cbb0115e
tom-mats <dawn.gray.sky@gmail.com>
2016-03-09 01:07:53 +0900

    record addition process

--- lib/inc_dec_record.rb
+++ lib/inc_dec_record.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module IncDecRecord
   class Hooks < Redmine::Hook::Listener
     def controller_issues_new_after_save(context={})
@@ -5,6 +7,12 @@ module IncDecRecord
     def controller_issues_edit_after_save(context={})
       c_journal = context[:issue].journals.select{|elem| elem.id == context[:journal].id}
       p c_journal[0].details
+      today_data = DailyVariation.all.select { |elem| elem.date == Date.today && elem.priority == 1}
+      today_data = [DailyVariation.create(date: Date.today, project: 1, priority: 1, count: 0)] if today.empty?
+      today_data.each {|data|
+        data.increment(:count)
+        data.save
+      }
     end
   end
 end
