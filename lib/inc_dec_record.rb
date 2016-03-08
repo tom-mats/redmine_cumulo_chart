30466b33ffbd327ddf83e738750a797e5d454dd3
tom-mats <dawn.gray.sky@gmail.com>
2016-03-09 01:16:34 +0900

    use where instead of select

--- lib/inc_dec_record.rb
+++ lib/inc_dec_record.rb
@@ -7,8 +7,8 @@ module IncDecRecord
     def controller_issues_edit_after_save(context={})
       c_journal = context[:issue].journals.select{|elem| elem.id == context[:journal].id}
       p c_journal[0].details
-      today_data = DailyVariation.all.select { |elem| elem.date == Date.today && elem.priority == 1}
-      today_data = [DailyVariation.create(date: Date.today, project: 1, priority: 1, count: 0)] if today.empty?
+      today_data = DailyVariation.where(project: 1 and priority: 1 and date: Date.today)
+      today_data = DailyVariation.create(date: Date.today, project: 1, priority: 1, count: 0) if today.empty?
       today_data.each {|data|
         data.increment(:count)
         data.save
