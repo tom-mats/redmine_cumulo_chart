2d06720dfa4748270f70f2b6ea86e93ad0f0bd44
tom-mats <dawn.gray.sky@gmail.com>
2016-04-03 22:41:09 +0900

    add test chart

--- lib/view_project_top_hooks.rb
+++ lib/view_project_top_hooks.rb
@@ -2,8 +2,16 @@ class ShowChart < Redmine::Hook::ViewListener
   def view_projects_show_left(context={})
     tags = []
     tags << javascript_include_tag('plotly.js', :plugin => 'redmine_cumulo_chart', :media => 'all')
-    html = ""
-    html += "<p>view_project_show_left</p>"
+    html = <<"EOS"
+    <script type="text/javascript">
+    var trace1 = {
+      x : [1, 2, 3, 4, 5]
+      y : [3, 5, 8, 9, 10]
+    };
+    var data = [trace1];
+    Ploty.new('chart', data);
+    </script>
+    html += '<div class="chart" style="width:90% height:260px"></div>'
     return tags.join(' ') + html
   end
 end
