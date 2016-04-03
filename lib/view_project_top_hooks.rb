baf03c4c02d3ee96f084266b254c9f2d34fc00c1
tom-mats <dawn.gray.sky@gmail.com>
2016-04-03 22:32:04 +0900

    fix typo

--- lib/view_project_top_hooks.rb
+++ lib/view_project_top_hooks.rb
@@ -1,7 +1,7 @@
 class ShowChart < Redmine::Hook::ViewListener
   def view_projects_show_left(context={})
     tags = []
-    tags << javascript_include_tag('plotly.js', :plugin => 'redmine_cumulo_chart', :media = 'all')
+    tags << javascript_include_tag('plotly.js', :plugin => 'redmine_cumulo_chart', :media => 'all')
     html = ""
     html += "<p>view_project_show_left</p>"
     return tags.join(' ') + html
